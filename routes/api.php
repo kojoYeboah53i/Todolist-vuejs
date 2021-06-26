8d2d03536b8842fad1569444513b9f8a8ab1a38b
kojoyeboah53i <yeboah.determined.isaac@gmail.com>
2021-06-26 14:58:14 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('item', ItemController::class, ['except' => ['create', 'show', 'edit']]);
+
+Route::get('/items', [ItemController::class, 'index']);
+
+Route::prefix('/item')->group( function () {
+    Route::post('/store', [ItemController::class, 'store']);
+    Route::put('/{id}', [ItemController::class, 'update']);
+    Route::delete('/{id}', [ItemController::class, 'destroy']);
+  
+    }
+);

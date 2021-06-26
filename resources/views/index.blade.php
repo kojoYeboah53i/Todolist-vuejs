472acdd5066028eb9921c7fd590b3bca77d318d1
kojoyeboah53i <yeboah.determined.isaac@gmail.com>
2021-06-26 15:19:59 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,68 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Todo App (Laravel8 + vue3)</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-</head>
-
-<body>
-    <div id="app">
-        <app></app>
+<template>
+  
+    <h1 id="title"> Add Todo List</h1>
+   
+    <div class="todoListContainer">
+      <div class="heading">
+        <add-item-form @reloadlist="getList()"/>
+      </div>
+      <list-view :items="items" @reloadlist="getList()"/>
     </div>
-    <script src="{{ mix('js/app.js') }}"></script>
-</body>
-
-</html>
+  </template>
+  
+  <script>
+  import addItemForm from "./addItemForm";
+  import listView from "./listView";
+  export default {
+    components: {
+      addItemForm,
+      listView,
+    },
+    data() {
+      return {
+        items: [],
+      };
+    },
+    methods: {
+      getList() {
+        axios.get("./api/items")
+          .then((response) => {
+            this.items = response.data;
+            console.log("data returned")
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+    },
+    created() {
+      this.getList();
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .todoListContainer {
+    text-align: center;
+    width: 350px;
+    margin: auto;
+    position:relative;
+    top: 10px;
+    border-radius: 35px;
+    box-shadow: 0 2px 10px 1px rgba(44, 39, 39, 0.3);
+  
+  }
+  .heading {
+    background: #e6e6e6;
+    padding: 10px;
+    border-radius: 35px  35px 0 0;
+    background: #062723;
+    
+  
+  }
+  #title {
+    margin-top: 100px;
+    margin-bottom: 0;;
+    text-align: center;
+  }
+  </style>
